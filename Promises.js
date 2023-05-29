e14b8e773b6a7157e3f3606f7bf93f437be06b3d
Binitstha <114696626+Binitstha@users.noreply.github.com>
2023-05-29 20:15:16 +0545

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -1,20 +1,22 @@
-// let promise = new Promise(function (resolve, reject) {
-//     let x = true
-//     if (x) {
-//         resolve("X gives true boolean value")
-//     }
-//     else {
-//         reject("X gives false boolean value")
-//     }
-// })
-// promise.then(
-//     function (value) {
-//         console.log(value)
-//     },
-//     function (error) {
-//         console.log(error)
-//     }
-// )
+let promise = new Promise(function (resolve, reject) {
+    let x = true
+    if (x) {
+        resolve("X gives true boolean value")
+    }
+    else {
+        reject("X gives false boolean value")
+    }
+})
+promise.then(
+    function (value) {
+        console.log(value)
+    },
+    function (error) {
+        console.log(error)
+    }
+)
+
+// Another short cut way with arrow funtion
 let promise = new Promise(resolve => {
     let x = true
     if (x) {
